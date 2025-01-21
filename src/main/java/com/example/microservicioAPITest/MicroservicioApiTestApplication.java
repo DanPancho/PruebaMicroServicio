2d2fc989b1597836b88240f5bed2fffbb9c1b8b4
DanPancho <daarcosa@puce.edu.ec>
2025-01-21 09:28:18 -0500

    master: init commit

---
+++ src/main/java/com/example/microservicioAPITest/MicroservicioApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicioAPITest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicioAPITest/MicroservicioApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicioAPITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
